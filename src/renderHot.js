ecfc4449677144e52c7e4934972d3d42d70867a2
Chris Abrams <mail@chrisabrams.com>
2018-01-06 13:12:48 -0500

    this console.log does not fire

--- src/renderHot.js
+++ src/renderHot.js
@@ -3,9 +3,8 @@ import { hot } from 'react-hot-loader'
 
 export default hot(module)(function renderHot(Component) {
 
-  // Whether you return the Component directly or wrapped in a function it is still a object on return
-  const Wrapped = () => Component
-
-  return Wrapped
+  // This does not fire
+  console.log('typeof inside', typeof Component)
+  return Component
 
 })
