9150346c87335ad6aaa79605506610ed707221bb
Chris Abrams <mail@chrisabrams.com>
2018-01-06 13:21:36 -0500

    i really dont fire here

--- src/renderHot.js
+++ src/renderHot.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 
 export default hot(module)(function renderHot(Component) {
+  console.log('helloo?????')
+  const Wrapped = () => Component
 
-  // This does not fire
-  console.log('typeof inside', typeof Component)
-  return Component
+  return Wrapped
 
 })
